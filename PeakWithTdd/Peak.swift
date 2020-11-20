a67b0f5d61db509c24c9f66e12de6c36fd1ccf03
mnem <bulatkin@bk.ru>
2020-11-20 23:37:09 +0300

    Update findPeak

--- PeakWithTdd/Peak.swift
+++ PeakWithTdd/Peak.swift
@@ -9,7 +9,28 @@ import Foundation
 
 class Peak {
     func findPeak(in array: [Int]) -> Int? {
-        let arraySorted = array.sorted()
-        return arraySorted.last
+        var maxElement = 0
+        var left = 0
+        var right = array.count - 1
+        var middle = 0
+        
+        while left <= right {
+            middle = Int(floor(Double(left + right) / 2.0))
+            if array[middle] < array[middle + 1] {
+                left = middle + 1
+                if maxElement < array[middle] {
+                    maxElement = array[middle]
+                }
+            } else if array[middle] < array[middle - 1] {
+                right = middle - 1
+                if maxElement < array[middle] {
+                    maxElement = array[middle]
+                }
+            } else {
+                maxElement = array[middle]
+                break
+            }
+        }
+        return maxElement
     }
 }
